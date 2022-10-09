50ea25f07f5c705b0e10c5fd5f2e8b73ac919429
Farrukh Adeel <81711287+FarrukhAdeel67@users.noreply.github.com>
2022-10-09 16:19:05 +0500

    updated

--- routes/driver.js
+++ routes/driver.js
@@ -4,11 +4,28 @@ const router = Router();
 // Controllers
 const controller = require("../controllers/driver");
 
-router.post("/", controller.createDriver )
-router.get("/", controller.allPosts )
+//image upload
+const multer = require("multer");
+const path = require("path");
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "uploads");
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + "-" + Date.now() + ".jpg");
+    },
+  }),
+}).array("licence_images");
+
+router.post("/pickParcel", controller.selectParcel);
+router.post("/feedback", controller.feedback);
+router.post("/", controller.createDriver);
+router.get("/", controller.allPosts);
 router.get("/driverProfile", controller.driverProfile);
-
-router.post("/profile", controller.profilePost)
-router.get("/profile", controller.profileGet);
-
+router.post(
+  "/driverProfile/uploadLicenseImages",
+  upload,
+  controller.profilePost
+);
 module.exports = router;
--- routes/homePage.js
+++ routes/homePage.js
@@ -1,12 +1,10 @@
 const { Router } = require("express");
 const router = Router();
 
-// Controllers
-const controller = require("../controllers/user");
-const clientController = require("../controllers/client");
 //Routers
 const driverRouter = require("./driver");
 const clientRouter = require("./client");
+
 router.use("/clientHomePage", clientRouter);
 router.use("/driverHomePage", driverRouter);
 
